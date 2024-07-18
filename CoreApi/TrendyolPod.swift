ce826ac4a0a3b6cbed5f165a52e3adbaf0ae5219
dogukanahi <bf427@hotmail.com>
2024-07-18 10:18:46 +0300

    Tests added

---
+++ CoreApi/TrendyolPod.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+public final class TrendyolPod {
+    let name = "Trendyol Pod"
+    
+    public func sum(x:Int, y:Int) -> Int {
+        return x + y
+    }
+    public func multiply(x:Int, y:Int) -> Int {
+        return x * y
+    }
+}
--- CoreApiTests/CoreApiTests.swift
+++ CoreApiTests/CoreApiTests.swift
@@ -10,27 +10,17 @@ import XCTest
 
 final class CoreApiTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var trendyolPod : TrendyolPod!
+    
+    override  func setUp() {
+        trendyolPod = TrendyolPod()
+        
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testSum() {
+        XCTAssertEqual((trendyolPod.sum(x: 2, y: 3)), 5)
+        
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testMultiply() {
+        XCTAssertEqual((trendyolPod.multiply(x: 2, y: 3)), 6)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
